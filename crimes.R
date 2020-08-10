4a12a29785e6a1461518b5cd9aa8cdebdc76a84e
zeadolfo <zeadolfo@gmail.com>
2020-08-10 16:27:37 -0500

    .

--- crimes.R
+++ crimes.R
@@ -21,6 +21,8 @@ library(dplyr)
 library(doBy)
 library(cluster)
 library(ggcorrplot)
+library(gstat)
+
 source("http://www.sthda.com/upload/rquery_cormat.r")
 rm(list = ls())
 
@@ -28,8 +30,9 @@ rm(list = ls())
 setwd("/home/ze/Área de Trabalho/Bases de dados/Crimes Boston/crimesBoston/")
 dados <- fread("crime.csv")
 mapa <- readOGR("Boston_Neighborhoods.geojson")
+pobreza <- fread("pobreza_Boston.csv", header = T, dec = ",")
 
-
+pobreza <- pobreza[-24]
 
 dados[,.N, by = "STREET"]
 dados[, .N, by = "MONTH"][order(MONTH)]
@@ -50,7 +53,7 @@ dados <- dados[Long != -1]
 dados_crimenes <- dados[,.N, c("Long", "Lat", "OFFENSE_CODE_GROUP")]
 
 coord <- SpatialPoints(data.frame(Long = dados_crimenes$Long, Lat = dados_crimenes$Lat))
-crimes <- SpatialPointsDataFrame(data.frame(Long = dados_crimenes$Long, Lat = dados_crimenes$Lat))
+# crimes <- SpatialPointsDataFrame(data.frame(Long = dados_crimenes$Long, Lat = dados_crimenes$Lat))
 
 crimes_reshape <- dcast(data = dados_crimenes, Long + Lat ~ OFFENSE_CODE_GROUP, value.var = "N")
 
@@ -61,7 +64,7 @@ crimes_coors <- crimes_reshape[,1:2]
 crimes_data <- crimes_reshape[,3:69]
 
 crimes_data <- apply(crimes_data, 2, f <- function(x) ifelse(is.na(x), 0 , x))
-crimes_data <- SpatialPointsDataFrame(crimes_coors, data = data.frame(crimes_data), proj4string = CRS(proj4string(mapa2)))
+crimes_data <- SpatialPointsDataFrame(crimes_coors, data = data.frame(crimes_data), proj4string = CRS(proj4string(mapa)))
 dados_tipo <- dados[,.N, by = c("Long", "Lat", "OFFENSE_CODE_GROUP")]
 dados_mes <- dados[,.N, by = c("Long", "Lat", "MONTH", "OFFENSE_CODE_GROUP")]
 dados_tipo[,N := as.numeric(N)]
@@ -79,25 +82,21 @@ res <- cbind(res, crimes_data@data)
 crimes_dist <- summaryBy(data = res, res[,8:74] ~ Name, FUN = sum)
 
 
-
 mat_cor <- cor(crimes_dist[,-1], method = "spearman")
-
+print(mat_cor)
 #There are more than 27 variables. So, it's important to know if it the variables are really important
 
 ola <- apply(crimes_dist[,-1], 2, f <- function(x){(x - min(x))/(max(x)-min(x))})
 comp_prin <- prcomp(ola)
 summary(comp_prin)
-#we need only five components instead of 60 variables
+#we need only five components instead of 27 variables
 
 
-conteo <- data.table(res)[!is.na(Name),.N, by = "Name"]
-conteo <- merge(conteo, mapa@data, by = "Name", all.x = T, all.y = F)
+# conteo <- data.table(res)[!is.na(Name),.N, by = "Name"]
+# conteo <- merge(conteo, mapa@data, by = "Name", all.x = T, all.y = F)
 
 
-#######################################################################################333
 
-
-res <- over(crimes_data, mapa)
 
 
 
@@ -118,7 +117,9 @@ mapa2 <- SpatialPolygonsDataFrame(mapa, data = data.frame(mapa@data))
 crimes <-  data.frame(Long = dados_tipo[OFFENSE_CODE_GROUP == "Homicide"]$Long, Lat = dados_tipo[OFFENSE_CODE_GROUP == "Homicide"]$Lat)
 crimes <- SpatialPointsDataFrame(coords = crimes, data = data.frame(id=1:dim(crimes)[1]), proj4string = CRS(proj4string(mapa2)))
 
-conteo <- dados_tipo[OFFENSE_CODE_GROUP == "Homicide",.N, by = "Name"]
+res <- over(crimes, mapa2)
+
+conteo <- data.table(res)[,.N,by = "Name"]
 
 mapa2@data <- merge(mapa2@data, conteo, by = "Name", all.x = T, all.y = F)
 mapa2@data$N <- ifelse(is.na(mapa2@data$N) , 0, mapa2@data$N)
@@ -131,7 +132,8 @@ tm_shape(mapa2) + tm_fill(col = "N") + tm_borders()
 homicidios <- as.geodata(dados_tipo[OFFENSE_CODE_GROUP == "Homicide"], coords.col = 1:2, data.col = 4)
 
 
-#We can see the homicides are related with the distance, i.e., longer, more improbable to have 
+#We can see the homicides are related with the distance
+
 
 res1.v <- variog(homicidios, trend = "1st")
 plot(res1.v, type = "b")
@@ -142,10 +144,21 @@ plot(res1.v, env = mc1, xlab = "u")
 mc2 <- variog.mc.env(homicidios, obj = res2.v)
 plot(res2.v, env = mc2, xlab = "u")
 
-
+#By the variogram, the homicides happen until some distance. 
 
 modelo1 <- likfit(homicidios, trend = "1st", ini.cov.pars = c(1,1))
-modelo2 <- likfit(homicidios, trend = "1st", ini.cov.pars = c(1,1),cov.model = "matern", kappa = 1.5)  
+KC <- krige.control(obj.model = modelo1)
+OC <- output.control(n.pred = 1000, simul = TRUE, thres = 250)
+
+#Simulate some coordinates
+coordenadas <- pred_grid(t(mapa@bbox), by = 0.01)
+
+#Kriging using the model above
+pred <- krige.conv(homicidios, locations = coordenadas, krige = KC, output = OC)
+
+
+#All the predictions are more than 1 and less than 2. It is expected because thare are no points with 0 and a few points with 2
+
 
 #In both cases it is better the geo model 
 
@@ -163,140 +176,40 @@ mapa2 <- SpatialPolygonsDataFrame(mapa, data = data.frame(mapa@data))
 crimes <-  data.frame(Long = dados_tipo[OFFENSE_CODE_GROUP == "Robbery"]$Long, Lat = dados_tipo[OFFENSE_CODE_GROUP == "Robbery"]$Lat)
 crimes <- SpatialPointsDataFrame(coords = crimes, data = data.frame(id=1:dim(crimes)[1]), proj4string = CRS(proj4string(mapa2)))
 
-res <- over(crimes, mapa)
+res <- over(crimes, mapa2)
 
 res <- data.table(res)[,.N, by = "Name"]
 mapa2@data <- merge(mapa2@data, res, by = "Name", all.x = T, all.y = F)
 mapa2@data$N <- ifelse(is.na(mapa2@data$N) , 0, mapa2@data$N)
 tm_shape(mapa2) + tm_fill(col = "N") + tm_borders()
 
+mapa2@data <- merge(mapa2@data, pobreza, all.x = T, all.y = F, by.x = "Name", by.y = "District")
 #Robberies happened more often in the Dorchester and Roxbury 
 
+cor(mapa2@data$N, mapa2@data$pobreza, use = "complete.obs", method = "spearman")
+cor(mapa2@data$N, mapa2@data$Impoverished, use = "complete.obs", method = "spearman")
+
+
 robbery <- as.geodata(dados_tipo[OFFENSE_CODE_GROUP == "Robbery"], coords.col = 1:2, data.col = 4)
+# robbery <- dados_tipo[OFFENSE_CODE_GROUP == "Robbery",]
 
-
-plot(variog(robbery))
-
-#We can see the homicides are related with the distance, i.e., longer, more improbable to have 
 
 res1.v <- variog(robbery, trend = "1st")
 plot(res1.v, type = "b")
 res2.v <- variog(robbery, trend = "2nd")
 lines(res2.v, type = "b", lty = 2)
-mc1 <- variog.mc.env(robbery, obj = res1.v)
-plot(res1.v, env = mc1, xlab = "u")
-mc2 <- variog.mc.env(robbery, obj = res2.v)
-plot(res2.v, env = mc2, xlab = "u")
-
-modelo1 <- likfit(robbery, trend = ~factor(MONTH), ini.cov.pars = c(1,1),cov.model = "matern", kappa = 1.5)
-modelo2 <- likfit(robbery, trend = "1st", ini.cov.pars = c(1,1),cov.model = "matern", kappa = 1.5)
-
-
-locales <- robbery$coords
-KC <- krige.control(type = "sk", obj.mod = modelo1)
-sk <- krige.conv(car_lacerny, krige = KC, loc = locales)
-KCt <- krige.control(type = "sk", obj.mod = modelo2)
-skt <- krige.conv(car_lacerny, krige = KCt, loc = locales)
-
-
-
-
-
-
-
-
-
-
-
-
-
-###### We know the importance of the weekends in the violence
-###### Now, we change a little 
-
-dados[OFFENSE_CODE_GROUP == "Larceny", .N, by = "HOUR"][order(HOUR)]
-dados[OFFENSE_CODE_GROUP == "Towed", .N, by = "HOUR"][order(HOUR)]
-
-dados_hora <- dados[,.N, by = c("Long", "Lat", "HOUR", "OFFENSE_CODE_GROUP")]
-
-crimes <-  data.frame(Long = dados_hora[OFFENSE_CODE_GROUP == "Larceny"]$Long, Lat = dados_hora[OFFENSE_CODE_GROUP == "Larceny"]$Lat)
-crimes <- SpatialPoints(crimes)
-plot(mapa)
-plot(crimes, add = T, col = "blue")
-
-
-######## First hour
-
-mapa2 <- SpatialPolygonsDataFrame(mapa, data = data.frame(mapa@data))
-crimes <-  data.frame(Long = dados_hora[OFFENSE_CODE_GROUP == "Larceny" & HOUR == 1]$Long, Lat = dados_hora[OFFENSE_CODE_GROUP == "Larceny" & HOUR == 1]$Lat)
-crimes <- SpatialPointsDataFrame(coords = crimes, data = data.frame(id=1:dim(crimes)[1]), proj4string = CRS(proj4string(mapa2)))
-
-res <- over(crimes, mapa)
-
-res <- data.table(res)[,.N, by = "Name"]
-mapa2@data <- merge(mapa2@data, res, by = "Name", all.x = T, all.y = F)
-mapa2@data$N <- ifelse(is.na(mapa2@data$N) , 0, mapa2@data$N)
-tm_shape(mapa2) + tm_fill(col = "N") + tm_borders()
-
-
-###### Second hour
-
-mapa2 <- SpatialPolygonsDataFrame(mapa, data = data.frame(mapa@data))
-crimes <-  data.frame(Long = dados_hora[OFFENSE_CODE_GROUP == "Larceny" & HOUR == 2]$Long, Lat = dados_hora[OFFENSE_CODE_GROUP == "Larceny" & HOUR == 2]$Lat)
-crimes <- SpatialPointsDataFrame(coords = crimes, data = data.frame(id=1:dim(crimes)[1]), proj4string = CRS(proj4string(mapa2)))
-
-res <- over(crimes, mapa)
-
-res <- data.table(res)[,.N, by = "Name"]
-mapa2@data <- merge(mapa2@data, res, by = "Name", all.x = T, all.y = F)
-mapa2@data$N <- ifelse(is.na(mapa2@data$N) , 0, mapa2@data$N)
-tm_shape(mapa2) + tm_fill(col = "N") + tm_borders()
-
-
-
-###### 3AM
-
-mapa2 <- SpatialPolygonsDataFrame(mapa, data = data.frame(mapa@data))
-crimes <-  data.frame(Long = dados_hora[OFFENSE_CODE_GROUP == "Larceny" & HOUR == 3]$Long, Lat = dados_hora[OFFENSE_CODE_GROUP == "Larceny" & HOUR == 3]$Lat)
-crimes <- SpatialPointsDataFrame(coords = crimes, data = data.frame(id=1:dim(crimes)[1]), proj4string = CRS(proj4string(mapa2)))
-
-res <- over(crimes, mapa)
-
-res <- data.table(res)[,.N, by = "Name"]
-mapa2@data <- merge(mapa2@data, res, by = "Name", all.x = T, all.y = F)
-mapa2@data$N <- ifelse(is.na(mapa2@data$N) , 0, mapa2@data$N)
-tm_shape(mapa2) + tm_fill(col = "N") + tm_borders()
-
-
-
-
-
-
-
-
-
-
-
-lacerny <- as.geodata(dados_semana[OFFENSE_CODE_GROUP == "Larceny"], coords.col = 1:2, data.col = 4, covar.col = 2)
-
-plot(variog(car_lacerny))
+res3.v <- variog4(robbery)
+plot(res3.v, type = "b")
 
 #We can see the homicides are related with the distance, i.e., longer, more improbable to have 
-
-res1.v <- variog(car_lacerny, trend = "1st")
-plot(res1.v, type = "b")
-res2.v <- variog(car_lacerny, trend = "2nd")
-lines(res2.v, type = "b", lty = 2)
-# mc1 <- variog.mc.env(car_lacerny, obj = res1.v)
-# plot(res1.v, env = mc1, xlab = "u")
-# mc2 <- variog.mc.env(car_lacerny, obj = res2.v)
-# plot(res2.v, env = mc2, xlab = "u")
-
-modelo1 <- likfit(car_lacerny, trend = "1st", ini.cov.pars = c(1,1))
-modelo2 <- likfit(car_lacerny, trend = "1st", ini.cov.pars = c(1,1),cov.model = "matern", kappa = 1.5)
+#We can see the trend the second order, apparently, does not have difference with the first order
 
 
-locales <- car_lacerny$coords
-KC <- krige.control(type = "sk", obj.mod = modelo1)
-sk <- krige.conv(car_lacerny, krige = KC, loc = locales)
-KCt <- krige.control(type = "sk", obj.mod = modelo2)
-skt <- krige.conv(car_lacerny, krige = KCt, loc = locales)
+MC <- model.control()
+PC <- prior.control(phi.discrete = seq(0, 6, l = 21), phi.prior = "reciprocal", tausq.rel.prior = "unif", tausq.rel.discrete = seq(0, 1, l = 11))
+OC <- output.control(n.post = 1000, moments = T)
+skb <- krige.bayes(robbery, loc = coordenadas, model = MC, prior = PC, output = OC)
+#The model has a singular matrix because the most positions have only one robbery
+
+
+
